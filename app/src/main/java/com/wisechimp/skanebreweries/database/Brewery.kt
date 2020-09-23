87c18ca940bc4e98c2bb8678c69dd2b31b83a9d1
shicks1080 <stuart.hicks@gmail.com>
2020-09-23 14:39:47 +0200

    Adding an id to the json data
    As I'm not pushing the data to Firebase then it doesn't get a random id so I've had to insert my own.

--- app/src/main/java/com/wisechimp/skanebreweries/database/Brewery.kt
+++ app/src/main/java/com/wisechimp/skanebreweries/database/Brewery.kt
@@ -1,6 +1,7 @@
 package com.wisechimp.skanebreweries.database
 
 data class Brewery(
+    var id: Int = 0,
     var name: String = "",
     var description: String = "",
     var founded: String = "",
